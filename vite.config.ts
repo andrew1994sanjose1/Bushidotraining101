a3dd2647ab5666a2bba915aa78c2523fca1548bd
andrew1994sanjose1 <andrew1994sanjose@gmail.com>
2026-04-27 12:43:27 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Nilinis natin ang config para sa final deployment
   server: {
     host: true,
     port: 5173,
@@ -13,7 +10,6 @@ export default defineConfig({
     ]
   },
   build: {
-    // Sinisiguro nito na ang output folder ay laging 'dist'
     outDir: 'dist',
     sourcemap: false
   }
